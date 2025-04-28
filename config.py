efec31e9a38133af69498ee4aeed347aca4a986b
Jeff Kenney <kenney@usna.edu>
2025-04-28 10:29:31 -0400

    Example configuration file

---
+++ config.py
@@ -0,0 +1,51 @@
+# Sanic Framework Configurations
+
+# Sample config.file
+
+site_settings = {
+    'NAME':           "SanicApp_Version_1",  # Title of Application
+    'API_LOCATIONS': [
+                      'api/auth/general',    # General Authentication (Required)
+                      'api/auth/pam',        # PAM (Local Logon) Support
+                      'api/auth/apikey',     # APIKey and APIFile-Upload Support
+                      'api/auth/email'       # Email Challenge Support
+                      ],
+    'DEFAULT_SCHOOL': 'XXXX'                 # Default School for Schedules
+    #'HSTS':'86400'                          # Use if you want to set HSTS headers
+}
+
+db_settings = {
+    'DB_HOST': '127.0.0.1',                  # MySQL Database Host
+    'DB_USER': 'username',                   # MySQL Database User
+    'DB_PASS': 'password',                   # MySQL User Password
+    'DB_NAME': 'dbname',                     # MySQL Database/Schema Name
+    'DB_PORT':  3306                         # MySQL Port
+}
+
+memcached_settings = {
+    'MEMCACHED_SERVER': '127.0.0.1',         # Memcached Server
+    'MEMCACHED_PORT':    11211               # Memcached Port
+}
+
+web_settings = {
+    'SHOW_SITE_CONTENTS':         True,                       # Allow directory listings
+    # The following values are located relative to server.py's filesystem location
+    'HTML':                      './html/',                   # Location of html directory
+    'INITIAL_PAGE':              './html/index.html',         # Initial start page
+    'FAVICON':                   './html/favicon.ico',        # Site icon
+    # The following values are located relative
+    'REDIRECT_LOGON_SUCCESSFUL': '/html/index.html',          # Redirect after successful logon
+    'REDIRECT_LOGON_FAILED':     '/auth/logon/apifile.html',  # Redirect after unsuccessful logon
+    'REDIRECT_LOGOFF':           '/html/index.html',          # Redirect after logoff
+    'CHALLENGE_PAGE':            '/auth/logon/challenge.html',# Page for challenges
+    'LOGON_LOGO':                '/auth/logon/logo.webp'      # Large image for logon pages
+}
+
+auth_settings = {
+    'AUTH_VALID':       604800,               # A Session is valid for 7 days
+    'AUTH_MESSAGE':    "",                    # Sign on Banner / Consent Banner
+    'AUTH_TITLE':      'SanicApp V1',         # Email Challenge - Title
+    'AUTH_EMAILER':    'noreply@example.com', # Email Challenge - Return Address
+    'AUTH_DOMAIN':     '@example.com',        # Email Challenge - Email Domain
+    'AUTH_EMAIL_SERVER': 'localhost'          # Email Challenge - Local SMTP Server
+}
