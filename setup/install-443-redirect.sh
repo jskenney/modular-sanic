03d006c79fbc1e493f099b91fd119670e36cd232
Jeff Kenney <kenney@usna.edu>
2025-04-28 10:31:27 -0400

    Example port 8000 to 443 redirect script

---
+++ setup/install-443-redirect.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Make sure that this provides only one response.
+DEST=$(ip a | grep -v inet6 | grep -v 127.0.0.1 | grep inet | tr -s ' ' | cut -d ' ' -f 3 | cut -d '/' -f 1)
+
+IPT="PREROUTING -p tcp --destination $DEST --dport 443 -j REDIRECT --to-ports 8000"
+
+sudo /usr/sbin/iptables -t nat -A $IPT
+
+sudo apt update
+sudo apt install -y iptables-persistent
