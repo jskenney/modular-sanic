eb874502a163c4439e26f8df13fdff54254329bd
J. Kenney <kenney@moboard.com>
2025-10-18 14:58:46 -0400

    cleanup example startup script

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 if [ ! -d venv ]; then
 
   # Create and source a Python 3.11 virtual environment
-  python3.11 -m venv venv
+  python3.11 -B -m venv venv
   source ./venv/bin/activate
 
   # Install required packages
@@ -14,20 +14,20 @@ if [ ! -d venv ]; then
   pip install sanic sanic-ext aiomysql python-pam six aiomcache cryptography mysqlclient pymemcache oracledb
 
   # There is a bug in sanic-session, so we need python 3.11 and the repo below until it is fixed
-  python -m pip install sanic_session@git+https://github.com/jskenney/sanic-session
+  python -B -m pip install sanic_session@git+https://github.com/jskenney/sanic-session
 
 else
 
   # Source the Python 3.11 virtual environment
   source ./venv/bin/activate
 
+  # Start Modular Sanic
+  SANIC_CONFIG_FILE=config.py python3 -B $(which sanic) server \
+        --debug \
+  	    --reload -R . \
+  	    --host=0.0.0.0
+
+  # Additional / Extra command line arguments and examples:
+  # sanic server --fast --no-access-logs --reload --host=server.example.com -R ../site/ -R ../auth/ --cert=../ssl/bundle.crt --key=../ssl/bundle.key
+
 fi
-
-# Start Modular Sanic
-sanic server --debug --reload --host=0.0.0.0 -R .
-
-# The config file can be specified from the command line as well
-# SANIC_CONFIG_FILE=config.py sanic server --debug --reload --host=0.0.0.0 -R .
-
-# Example of use in Production
-# sanic server --fast --no-access-logs --reload --host=server.example.com -R ../site/ -R ../auth/ --cert=../ssl/bundle.crt --key=../ssl/bundle.key
