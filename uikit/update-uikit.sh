31a8760b80307f21a9aee832f6e996e60b6a36a5
J. Kenney <kenney@moboard.com>
2025-10-19 19:36:40 -0400

    use uikit main branch instead of develop branch

--- uikit/update-uikit.sh
+++ uikit/update-uikit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 rm -f uikit*
-wget https://raw.githubusercontent.com/uikit/uikit/refs/heads/develop/dist/css/uikit.min.css
-wget https://raw.githubusercontent.com/uikit/uikit/refs/heads/develop/dist/js/uikit.min.js
-wget https://raw.githubusercontent.com/uikit/uikit/refs/heads/develop/dist/js/uikit-icons.min.js
+wget https://raw.githubusercontent.com/uikit/uikit/refs/heads/main/dist/css/uikit.min.css
+wget https://raw.githubusercontent.com/uikit/uikit/refs/heads/main/dist/js/uikit.min.js
+wget https://raw.githubusercontent.com/uikit/uikit/refs/heads/main/dist/js/uikit-icons.min.js
 chmod 644 uikit*
 
