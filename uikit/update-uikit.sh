6fa1ebde6ab84727271ef0932f4594a159fb3ee2
Sanic Daemon <sanic@usna.edu>
2025-10-22 14:16:58 -0400

    update uikit setup

--- uikit/update-uikit.sh
+++ uikit/update-uikit.sh
@@ -4,5 +4,5 @@ rm -f uikit*
 wget https://raw.githubusercontent.com/uikit/uikit/refs/heads/main/dist/css/uikit.min.css
 wget https://raw.githubusercontent.com/uikit/uikit/refs/heads/main/dist/js/uikit.min.js
 wget https://raw.githubusercontent.com/uikit/uikit/refs/heads/main/dist/js/uikit-icons.min.js
-cat uikit*.js > uikit-all.min.js
+cat uikit.min.js uikit-icons.min.js > uikit-all.min.js
 chmod 644 uikit*
